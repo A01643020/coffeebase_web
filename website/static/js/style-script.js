415628fef9e14990001db17af3cba3098505aaa7
Sara <sarav.notes@gmail.com>
2024-03-21 11:05:16 -0600

    cambios led

--- website/static/js/style-script.js
+++ website/static/js/style-script.js
@@ -4,30 +4,38 @@ const titleC = document.querySelectorAll('.titleC');
 const number = document.querySelectorAll('.number');
 const svgIcon = document.querySelector('svg');
 
-switchInput.addEventListener('change', function() {
-    if (this.checked) {
-        svgIcon.classList.add('rotateON');
-        svgIcon.classList.remove('rotateOFF');
-        babyContainers.forEach(container => {
-            container.classList.add('active');
+switchInput.addEventListener('change', async function() {
+    // Determine the new value of LED based on the switch state
+    const newValue = this.checked ? 1 : 0;
+    try {
+        const response = await fetch('/api/updateLED', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ Led: newValue })
         });
-        titleC.forEach(element => {
-            element.classList.remove('hidden');
-        });
-        number.forEach(element => {
-            element.classList.remove('hidden');
-        });
-    } else {
-        svgIcon.classList.remove('rotateON');
-        svgIcon.classList.add('rotateOFF');
-        babyContainers.forEach(container => {
-            container.classList.remove('active');
-        });
-        titleC.forEach(element => {
-            element.classList.add('hidden');
-        });
-        number.forEach(element => {
-            element.classList.add('hidden');
-        });
+
+        if (!response.ok) {
+            throw new Error('Failed to update LED value');
+        }
+
+        // Handle UI changes based on the new LED value
+        if (this.checked) {
+            svgIcon.classList.add('rotateON');
+            svgIcon.classList.remove('rotateOFF');
+            babyContainers.forEach(container => container.classList.add('active'));
+            titleC.forEach(element => element.classList.remove('hidden'));
+            number.forEach(element => element.classList.remove('hidden'));
+        } else {
+            svgIcon.classList.remove('rotateON');
+            svgIcon.classList.add('rotateOFF');
+            babyContainers.forEach(container => container.classList.remove('active'));
+            titleC.forEach(element => element.classList.add('hidden'));
+            number.forEach(element => element.classList.add('hidden'));
+        }
+    } catch (error) {
+        console.error('Error updating LED value:', error);
     }
 });
+
